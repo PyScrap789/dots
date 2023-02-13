321b48ff4b8238a1d74a79cda30c2af00b3b5335
Nilansh Sharma <80972022+PyScrap789@users.noreply.github.com>
2023-02-13 14:25:45 +0530

    updated

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,9 +4,10 @@
 # If all your bars have ipc enabled, you can use 
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
-#killall -q polybar
+# killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bottom 2>&1 | tee -a /tmp/polybar2.log & disown
 echo "Bars launched..."
